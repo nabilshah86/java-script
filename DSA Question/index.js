033122bc64c95c4a4a668fa03b19bf452da16309
nabilshah86 <nabilshah698@gmail.com>
2025-02-04 03:26:25 -0800

    update

--- DSA Question/index.js
+++ DSA Question/index.js
@@ -74,3 +74,45 @@ function countVowels(str) {
     return count;
 }
 console.log(countVowels("hello world"))
+
+
+// 7. find the second largest number in an array.
+
+function secondLargest(arr){
+arr = [...new Set(arr)];
+arr.sort((a, b) => b - a);
+return arr[1];
+}
+console.log(secondLargest([10, 20, 4, 45, 99]));
+
+// 8. remove duplicate elements from an array and return a new array.
+
+function removeDuplicate(arr) {
+    return [...new Set(arr)];
+}
+console.log(removeDuplicate([1, 2, 2, 3, 4, 4, 5]));
+
+// 9. write a function to find the factorial of a given number using a loop.
+
+function factorial(n){
+    if (n === 0 || n === 1)
+
+    {
+        return 1; 
+    }
+    return n * factorial(n - 1);
+}
+console.log(factorial(5));
+
+// 10. write a function to check whether an array is sorted in ascending order.
+
+function isSorted(arr) {
+    for ( let i = 0; i < arr.length - 1; i++){
+        if (arr[i] > arr[i + 1]){
+            return false;
+        }
+    }
+    return true;
+}
+console.log(isSorted([1, 2, 3, 4, 5,]));
+console.log(isSorted([1, 3, 2, 4, 5,]));
